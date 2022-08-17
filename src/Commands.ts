837da28e9470c9fc8e77882e7b82505f9514b4be
godhw <shw0210@naver.com>
2022-08-17 17:43:09 +0900

    add help command

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,7 +1,8 @@
 import { Command } from './Command.js';
 import { Hello } from './commands/Hello.js';
 import { Generate } from './commands/Generate.js';
+import { Help } from './commands/Help.js';
 
-export const Commands: Command[] = [Hello, Generate];
+export const Commands: Command[] = [Hello, Generate, Help];
 
 export default Commands;
